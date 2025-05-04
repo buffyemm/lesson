f4a65184e4bfe70d34b822620c0836ab122f383b
Nikolay Cherevko <42761801@mail.ru>
2025-05-04 17:54:36 +0700

    step5, можно что то дописать

--- lesson.cpp
+++ lesson.cpp
@@ -13,14 +13,14 @@ struct location_ {      // создали структуру локации, в 
     vector<int>portal;
 
 };
-..
+
 location_ room[4];
 
 
 void InitGame() {       // создаем функцию которая будет заносить все наши значения в массив room
 
     room[0].name = "Дом";
-    room[0].portal.push_back(1);
+    room[0].portal.push_back(1); 
     room[1].name = "Комната";
     room[1].portal.push_back(0);
 
@@ -36,7 +36,7 @@ int main()
     
     cout << room[0].name << endl; // выведем имя локации 0
 
-    for (int i = 0; i < room[0].portal.size(); i++) {  // выведем порталы в какую локацию будут идти
+    for (int i = 0; i < room[0].portal.size(); i++) {  // выведем порталы, цифры будут обозначать в какую локацию будут идти
 
         cout << room[0].portal[i] << endl;
 
