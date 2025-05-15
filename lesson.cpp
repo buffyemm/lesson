d4603835929b598c4e3a52e8b7c3d0ddfcf87320
Nikolay Cherevko <42761801@mail.ru>
2025-05-16 01:12:56 +0700

    2.1

--- lesson.cpp
+++ lesson.cpp
@@ -16,7 +16,7 @@ struct location_ {      // создали структуру локации, в 
 location_ room[4];
 
 
-void InitGame() {       // создаем функцию которая будет заносить все наши значения в массив room
+void InitGame() {      // создаем функцию которая будет заносить все наши значения в массив room
 
     room[0].name = "Home";
     room[0].portal.push_back(1); 
@@ -35,7 +35,6 @@ int main()
     for (int i = 0; i < room[0].portal.size(); i++) {  // выведем порталы, цифры будут обозначать в какую локацию будут идти
 
         cout << room[0].portal[i] << endl;
-
 
     }
 
