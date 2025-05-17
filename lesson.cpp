9a7705a21a46d9733578bc5a9e6efbcd63cb74b6
Nikolay Cherevko <42761801@mail.ru>
2025-05-17 17:18:22 +0700

    DONE

--- lesson.cpp
+++ lesson.cpp
@@ -4,29 +4,26 @@
 int main()
 {   
     srand(time(NULL));//помошник для функции рандома, который принимает в себя время компьютера
-
     int user;// переменная user, типа int, целые переменные
-    int bot = 1 + rand() % 3; // в бот мы будем ставить функцию rand(), которая будет нам рандомить числа от 1 до 3
+    int bot = 1 + rand() % 9; // в бот мы будем ставить функцию rand(), которая будет нам рандомить числа от 1 до 9
 
-    std::cout << "Enter number\n1.rock\n2.scissors\n3.paper\n";
+    std::cout << "Enter number 1 - 9\n";
 
-    std::cin >> user; // принимает ввод с консоли и кладет наш ввод в переменную user
+    for (int i = 0; i < 10; i++) {
 
-    if (user == 1 && bot == 2 || user == 2 && bot == 3 || user == 3 && bot == 1) { // условия победы
 
-        std::cout << "win\n";
+        std::cin >> user; // принимает ввод с консоли и кладет наш ввод в переменную user
 
+        if (user == bot) { // условия победы
+
+            std::cout << " you win\n";
+            bot = 1 + rand() % 9;
+        }
+        else {
+
+            std::cout << "you lose\n"; // если условия выше не выполняются, значит поражение
+        }
     }
-    else if (user == bot) {// условия ничьи
-
-        std::cout << "draw\n";
-
-    }
-    else {
-
-        std::cout << "lose\n"; // если условия выше не выполняются, значит поражение
-
-    }
-
+            std::cout << "Bot number: " << bot << std::endl;
 }
 
