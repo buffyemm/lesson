017d2aad760a7438cf5935f7db13d797ac762385
Nikolay Cherevko <42761801@mail.ru>
2025-05-17 19:34:25 +0700

    DONE

--- lesson.cpp
+++ lesson.cpp
@@ -1,36 +1,80 @@
 ﻿#include <iostream>
 #include <string> // подключаем библиотеку для стринг
+#include <vector>
+
+struct product_ {
+
+    std::string name;
+    int price;
+    int count;
+
+};
+
+std::vector<product_> shop;
 
 int main()
 {   
-    srand(time(NULL));//помошник для функции рандома, который принимает в себя время компьютера
+    shop.push_back({ "Cheese", 250, 0 });
+    shop.push_back({ "Apple", 140, 10 });
+    shop.push_back({ "Potato", 40, 24 });
+    shop.push_back({ "Tomato", 260, 30 });
+    shop.push_back({ "Chips", 160, 70 });
+    shop.push_back({ "Bread", 40, 17 });
+    shop.push_back({ "Macaroni", 140, 43 });
 
-    std::string user;// переменная user становиться string, уже можем вмещать некие слова
+    int wallet = 2000; // кошелек пользлователя
+    int number;
+    int temp;
+    while (wallet > 0) {
 
-    int r = rand() % 3; // в r мы будем ставить функцию rand(), которая будет нам рандомить числа от 0 до 2, он будет выбирать позицию масива bot
+        
+        std::cout << "\nYour Wallet: " << wallet << "\tAssortment: \n";
+        std::cout << "Name\t\tPrice\tCount\n" << std::endl;
 
-    std::string bot[3]{ "rock", "scissors", "paper" };//создаем массив стринг,который будет считать двумерным массивом, и в него будем передавать r
+        for (int i = 0; i < shop.size(); i++) {
 
-    std::cout << "Enter word: \n1.rock\n2.scissors\n3.paper\n";
+            std::cout << i + 1 << ". " << shop[i].name << "\t" << shop[i].price << "\t" << shop[i].count << std::endl;
 
-    std::cin >> user; // принимает ввод с консоли и кладет наш ввод в переменную user
+        }
 
-    if (user == "rock" && bot[r] == "scissors" || user == "scissors" && bot[r] == "paper" || user == "paper" && bot[r] == "rock") { // условия победы
+        std::cout << "Chouse number\n";
 
-        std::cout << "win\n";
+        std::cin >> number;
 
+        if (shop[number - 1].count > 0) {
+
+            std::cout << "Chouse count\n";
+            std::cin >> temp;
+            if (shop[number - 1].count >= temp) {
+                if (shop[number - 1].price * temp < wallet) {
+
+                    wallet -= temp * shop[number - 1].price; 
+
+                    std::cout << "Buy: " << shop[number - 1].name << "\tTotal price: " << temp * shop[number - 1].price << std::endl;
+
+                    shop[number - 1].count -= temp;
+
+                    std::cout << "Wallet: " << wallet << std::endl;
+
+                }
+                else {
+
+                    std::cout << "NO MONEY\t Total PRICE: " << shop[number - 1].price * temp << " > " << wallet;
+
+                }
+            }
+            else {
+            
+                std::cout << "The quantity is less than the input\n";
+
+            }
+
+        }
+        else {
+
+            std::cout << "\nCount 0. It's over.\n";
+
+        }
     }
-    else if (user == bot[r]) {// условия ничьи
-
-        std::cout << "draw\n";
-
-    }
-    else {
-
-        std::cout << "lose\n"; // если условия выше не выполняются, значит поражение
-
-    }
-
-    std::cout << r << std::endl; // выводится число которое будет передаваться в массив bot
 }
 
